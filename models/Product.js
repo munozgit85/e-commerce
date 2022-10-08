0486dd0b005553e23755869d030fa0e319e0bbf7
Racquel Munoz <racquelg85@hotmail.com>
2022-10-08 17:54:33 -0500

    product info

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,44 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    //id column 
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    //product name column 
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    //price column 
+    price: {
+      type: DataTypes.DECIMAL, 
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    // stock column
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false, 
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    // category_id column
+    category_id: {
+      type: DataTypes.INTEGER,
+      // reference the category model's id 
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
